b52c8a82117f3f8e5fe93b9781bae1a201e43ee1
KenanMehmet <kenmehmet1@gmail.com>
2023-06-28 13:13:50 +0100

    working on search

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Button from './Button'
+
 
 const SearchExercises = () => {
     return (
-        <div>SearchExercises</div>
+        <div className='search-wrapper'>
+            <h1> Exercises you should <br /> know</h1>
+            <input
+                className='search-field'
+                type='text' value={""} onChange={(e) => { }}
+                placeholder='enter text'
+            ></input>
+        </div>
     )
 }
 
