54144ca53adc659e57fce57df49b88e88e5d9fa1
Kenan Mehmet <kenanmehmet@Kenans-MacBook-Pro.local>
2023-06-28 22:56:02 +0100

    made changes to button component and added rapid api

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 
+
 const getIconFromName = (iconName) => {
     console.log(iconName)
     switch (iconName) {
         case ('arrow-right'):
             return <AiOutlineArrowRight />;
         default:
-            return <p>Nothing</p>;
+            return false;
     }
 }
 
 
-const Button = ({ text, iconName, link }) => {
+const Button = ({ text, iconName, link, onClickfunction }) => {
     const icon = getIconFromName(iconName);
+    if (icon) {
+        return (
+            <button onClick={onClickfunction}>
+                <span className='button-text'>{text}</span>
+                <span className='button-icon'>{icon}</span>
+            </button>
+        )
+    }
     return (
         <button href={link}>
             <span className='button-text'>{text}</span>
-            <span className='button-icon'>{icon}</span>
         </button>
     )
 }
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,17 +1,32 @@
 import React, { useEffect, useState } from 'react'
 
 import Button from './Button'
+import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState('')
+
+    const handleSearch = async () => {
+        console.log("starting")
+        if (search) {
+            const exerciseData = fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
+            console.log(exerciseData)
+        }
+    }
+
+
     return (
         <div className='search-wrapper'>
             <h1> Exercises you should <br /> know</h1>
-            <input
-                className='search-field'
-                type='text' value={""} onChange={(e) => { }}
-                placeholder='enter text'
-            ></input>
+            <div style={{ display: "flex" }}>
+                <input
+                    className='search-field'
+                    type='text' value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())}
+                    placeholder='enter text'
+                />
+                <Button text="Search" onClickFucntion={handleSearch}></Button>
+            </div>
         </div>
     )
 }
